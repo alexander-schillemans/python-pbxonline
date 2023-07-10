a9ffa8a762bdf4f3e69878a295c1510b56b8d9ef
Alexander Schillemans <alexander.schillemans@lhs.global>
2023-07-10 16:22:24 +0200

    Add end call functionality

--- pbxonline/endpoints/action.py
+++ pbxonline/endpoints/action.py
@@ -23,3 +23,10 @@ class ActionEndpoint(APIEndpoint):
         if status > 299: return construct_error_from_data(resp_json)
         
         return construct_object_from_data(resp_json[0])
+    
+    def end_call(self, call_id) -> BaseModel:
+        
+        status, headers, resp_json = self.api.delete( f'{self.endpoint}/conversation', data={'ID' : call_id})
+        if status > 299: return construct_error_from_data(resp_json)
+        
+        return construct_object_from_data(resp_json)
